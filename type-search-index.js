4e7da8df3385d43488aa485bf976c11df3edde8e
Zilong WU <95653923+littlewuuu@users.noreply.github.com>
2023-01-26 12:22:22 +0100

    add javadoc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.ZilongKang","l":"Game"},{"p":"org.ZilongKang","l":"GameJudgeTest"},{"p":"org.ZilongKang","l":"GameTest"},{"p":"org.ZilongKang","l":"GuessJudge"},{"p":"org.ZilongKang","l":"Main"}];updateSearchResults();
