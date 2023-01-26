d7cc8b6c515986955a9499b16bad3c68d27cd1d3
Zilong WU <95653923+littlewuuu@users.noreply.github.com>
2023-01-26 09:29:46 +0100

    gameTest

--- src/main/java/org/ZilongKang/Main.java
+++ src/main/java/org/ZilongKang/Main.java
@@ -6,7 +6,6 @@ package org.ZilongKang;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("HelloWorld");
     }
 
 }
---
+++ src/test/java/org/ZilongKang/GameTest.java
@@ -0,0 +1,10 @@
+package org.ZilongKang;
+
+import org.junit.Test;
+
+public class GameTest {
+    @Test
+    public void GameInitiTest(){
+        System.out.println("aesf");
+    }
+}
